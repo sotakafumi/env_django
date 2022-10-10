1606323230573ecc666dfa50adf3d9595e3e668f
sotakafumi@outlook.jp <sotakafumi@outlook.jp>
2022-10-11 00:38:02 +0900

    master

---
+++ db/init/02_insert_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO book VALUES(DEFAULT,'プログラミング言語C',current_timestamp);
+INSERT INTO book VALUES(DEFAULT,'やさしいコンピュータ科学',current_timestamp);
+INSERT INTO book VALUES(DEFAULT,'ゲーデル、エッシャーバッハ－あるいは不思議の環',current_timestamp);
+INSERT INTO book VALUES(DEFAULT,'TeX ブック　コンピュータによる組版システム',current_timestamp);
+INSERT INTO book VALUES(DEFAULT,'人月の神話　オオカミ人間を撃つ銀の弾はなし',current_timestamp);
